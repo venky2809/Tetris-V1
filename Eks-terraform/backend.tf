f792887a36201324e2a74d50db2a752641602922
venkatesh Javvadi <venkateshjavvadi53@gmail.com>
2025-08-25 17:52:09 +0100

    changed

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "venkymaluma" # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
+    region = "eu-west-2"
   }
 }
