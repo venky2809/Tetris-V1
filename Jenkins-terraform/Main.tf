655ad410e10ca6aaee2f9f91647f5036d9689dd4
venkatesh Javvadi <venkateshjavvadi53@gmail.com>
2025-08-25 17:21:50 +0100

    changed

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,9 +59,9 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0df4b2961410d4cff"
+  ami                    = "ami-046c2381f11878233"
   instance_type          = "t2.medium"
-  key_name               = "mumbai-mac"
+  key_name               = "maluma"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
